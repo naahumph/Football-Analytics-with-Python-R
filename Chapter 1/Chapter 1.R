94202794824b172ec330a4a97f1431f55420a8ff
naahumph <55363317+naahumph@users.noreply.github.com>
2023-11-13 16:39:07 -0500

    Who Throws Deep example, R

---
+++ Chapter 1/Chapter 1.R
@@ -0,0 +1,23 @@
+## Chapter 1
+## Example: Who Throws Deep?
+
+## package installation
+install.packages("nflfastR")
+install.packages("tidyverse")
+
+## load the libraries to be used
+library("tidyverse")
+library("nflfastR")
+
+pbp_r <- load_pbp(2021)
+
+pbp_r_p <- 
+	pbp_r |> 
+	filter(play_type == 'pass' & !is.na(air_yards))
+
+pbp_r_p |> 
+	group_by(passer_id, passer) |>
+	summarize(n = n(), adot = mean(air_yards)) |>
+	filter(n >= 100 & !is.na(passer)) |>
+	arrange(-adot) |>
+	print(n = Inf)
