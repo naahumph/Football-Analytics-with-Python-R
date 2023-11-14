5eaa791527c830dc3f0f320b0e5e10ccfc140f23
naahumph <55363317+naahumph@users.noreply.github.com>
2023-11-14 14:27:15 -0500

    long/short creation and incomplete pass cleaning

--- Chapter 2/Chapter 2 - Book Work.py
+++ Chapter 2/Chapter 2 - Book Work.py
@@ -7,7 +7,7 @@ import numpy as np
 import nfl_data_py as nfl
 
 # obtaining and filtering data
-seasons = range(2016, 2022 + 1) # add 1 because python starts counting at 0
+seasons = range(2016, 2022 + 1) # add 1 because a python range does not include the 'stop' variable
 pbp_py = nfl.import_pbp_data(seasons)
 
 # filtering for just passing plays
@@ -15,4 +15,13 @@ pbp_py_p = \
     pbp_py\
     .query("play_type == 'pass' & air_yards.notnull()")\
     .reset_index()
+
+pbp_py_p['pass_length_air_yards'] = np.where(
+    pbp_py_p['air_yards'] >= 20, 'long','short'
+)
+
+pbp_py_p['passing_yards'] = \
+    np.where(
+        pbp_py_p['passing_yards'].isnull(), 0, pbp_py_p['passing_yards']
+        )
 
